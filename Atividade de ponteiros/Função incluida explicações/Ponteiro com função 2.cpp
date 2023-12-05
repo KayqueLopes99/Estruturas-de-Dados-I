41ba5b9f7d13e8fbc31424e0aeb9ffef4bef447c
Kayque Lopes <144949202+KayqueLopes99@users.noreply.github.com>
2023-12-05 15:02:08 -0300

    Update Ponteiro com função 2.cpp

--- Atividade de ponteiros/Função incluida explicações/Ponteiro com função 2.cpp
+++ Atividade de ponteiros/Função incluida explicações/Ponteiro com função 2.cpp
@@ -1,27 +1,27 @@
-//=========== ExplicaÁıes e entendimentos ================//
+//=========== Explica√ß√µes e entendimentos ================//
 
 /*#include <stdio.h>
 
 int main(void) {
-    int x, y; //vari·veis.
-    y = 2; //atribuiÁ„o de 2 a y.
+    int x, y; //vari√°veis.
+    y = 2; //atribui√ß√£o de 2 a y.
     int *p1, //*p2; Ponteiros.
-    p1 = &y; //EndereÁo de  y atribuido a p1.
-    x = 1; // atribuiÁ„o de 1.
-    p2 = p1; // contÈudo de p1 = p2.
+    p1 = &y; //Endere√ßo de  y atribuido a p1.
+    x = 1; // atribui√ß√£o de 1.
+    p2 = p1; // cont√©udo de p1 = p2.
     (*p1)++; // incremento
     printf("%d %d", x, y);
 
     return 0;
 }*/
-//(*p1)++:  o valor apontado por p1 È incrementado apÛs a operaÁ„o de desreferenciamento,
-//ou seja, numa conta n = i + (*p1)++. sendo i = 2, (*p1) = 5, o resultado n„o ser· 8 e sim 7.
-//E sim somente no valor impreso de p1 que ser· 6 com o incremento.
-//*p1++: Neste caso, significa que o valor apontado por p1 È obtido primeiro, ou seja, ele faz somente a soma normal
-//sem atribuiÁıes por enquanto.
-//e em seguida, o ponteiro p1 È incrementado.
-//++p1 atribui a incremento na operaÁ„o e no ponteiro.
-//n = i + (*p1)++. sendo i = 2, (*p1) = 5, logo o resultado ser· 8
+//(*p1)++:  o valor apontado por p1 √© incrementado ap√≥s a opera√ß√£o de desreferenciamento,
+//ou seja, numa conta n = i + (*p1)++. sendo i = 2, (*p1) = 5, o resultado n√£o ser√° 8 e sim 7.
+//E sim somente no valor imprimido de p1 que ser√° 6 com o incremento.
+//*p1++: Neste caso, significa que o valor apontado por p1 √© obtido primeiro, ou seja, ele faz somente a soma normal
+//sem atribui√ß√µes por enquanto.
+//e em seguida, o ponteiro p1 √© incrementado.
+//++p1 atribui a incremento na opera√ß√£o e no ponteiro.
+//n = i + (*p1)++. sendo i = 2, (*p1) = 5, logo o resultado ser√° 8
 
 /*EXEMPLOS DADO EM AULA.
 #include <stdio.h>
